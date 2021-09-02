1c4a86851ca0d79f993962ddb6f5e3d53e3246eb
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-09-01 23:09:24 -0300

    OrderItem, many-to-many association with extra attributes part 2

--- src/main/java/com/gustavo/course/config/TestConfig.java
+++ src/main/java/com/gustavo/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.gustavo.course.entities.Category;
 import com.gustavo.course.entities.Order;
+import com.gustavo.course.entities.OrderItem;
 import com.gustavo.course.entities.Product;
 import com.gustavo.course.entities.User;
 import com.gustavo.course.entities.enums.OrderStatus;
 import com.gustavo.course.repositories.CategoryRepository;
+import com.gustavo.course.repositories.OrderItemRepository;
 import com.gustavo.course.repositories.OrderRepository;
 import com.gustavo.course.repositories.ProductRepository;
 import com.gustavo.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -55,6 +60,7 @@ public class TestConfig implements CommandLineRunner {
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
+		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		
@@ -67,5 +73,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
